6fe4182616878a9c6cc49497168dae3f572df339
Joan Jara <joan.jara@estudiants.urv.cat>
2020-01-22 16:57:00 +0100

    Update
    Afegit codi per fer una commanda per provar.

---
+++ Practica2_JoanJara_AleixIglesias/src/java/command/InitCommand.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package command;
+
+import classes.Tenant;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
+import java.io.IOException;
+
+public class InitCommand implements Command {
+
+    @Override
+    public void execute(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // 1. process the request
+        request.setAttribute("user", new Tenant());
+
+        // 2. produce the view with the web result
+        ServletContext context = request.getSession().getServletContext();
+        context.getRequestDispatcher("/view.jsp").forward(request, response);
+    }
+}
